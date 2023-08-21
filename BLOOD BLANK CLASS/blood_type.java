cc9d89a0c3254f3be40b19257c9da43b5d506451
Geethika-30 <112071177+Geethika-30@users.noreply.github.com>
2023-08-21 13:11:40 +0530

    Add files via upload

---
+++ BLOOD BLANK CLASS/blood_type.java
@@ -0,0 +1,65 @@
+
+
+/**
+ * Class blood_type
+ */
+public class blood_type {
+
+  //
+  // Fields
+  //
+
+  private void ID;
+  private void bloodtype;
+  
+  //
+  // Constructors
+  //
+  public blood_type () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of ID
+   * @param newVar the new value of ID
+   */
+  private void setID (void newVar) {
+    ID = newVar;
+  }
+
+  /**
+   * Get the value of ID
+   * @return the value of ID
+   */
+  private void getID () {
+    return ID;
+  }
+
+  /**
+   * Set the value of bloodtype
+   * @param newVar the new value of bloodtype
+   */
+  private void setBloodtype (void newVar) {
+    bloodtype = newVar;
+  }
+
+  /**
+   * Get the value of bloodtype
+   * @return the value of bloodtype
+   */
+  private void getBloodtype () {
+    return bloodtype;
+  }
+
+  //
+  // Other methods
+  //
+
+}
