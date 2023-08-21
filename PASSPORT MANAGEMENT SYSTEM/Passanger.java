6b6424c6498d503d786b5c1e6fe5548cd6654b64
Geethika-30 <112071177+Geethika-30@users.noreply.github.com>
2023-08-21 13:15:40 +0530

    Add files via upload

---
+++ PASSPORT MANAGEMENT SYSTEM/Passanger.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class Passanger
+ */
+public class Passanger {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void phone_no;
+  private void email;
+  
+  //
+  // Constructors
+  //
+  public Passanger () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of phone_no
+   * @param newVar the new value of phone_no
+   */
+  private void setPhone_no (void newVar) {
+    phone_no = newVar;
+  }
+
+  /**
+   * Get the value of phone_no
+   * @return the value of phone_no
+   */
+  private void getPhone_no () {
+    return phone_no;
+  }
+
+  /**
+   * Set the value of email
+   * @param newVar the new value of email
+   */
+  private void setEmail (void newVar) {
+    email = newVar;
+  }
+
+  /**
+   * Get the value of email
+   * @return the value of email
+   */
+  private void getEmail () {
+    return email;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void apply_passport()
+  {
+  }
+
+
+  /**
+   */
+  public void submit_proof()
+  {
+  }
+
+
+}
