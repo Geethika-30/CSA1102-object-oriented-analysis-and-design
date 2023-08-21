486004e589764bcdb18da1d6ef67726e204937b7
Geethika-30 <112071177+Geethika-30@users.noreply.github.com>
2023-08-21 12:26:18 +0530

    Add files via upload

---
+++ reg_website.java
@@ -0,0 +1,120 @@
+
+
+/**
+ * Class reg_website
+ */
+public class reg_website {
+
+  //
+  // Fields
+  //
+
+  private void qualifications;
+  private void login_details;
+  private void fees;
+  private void exam_dates;
+  
+  //
+  // Constructors
+  //
+  public reg_website () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of qualifications
+   * @param newVar the new value of qualifications
+   */
+  private void setQualifications (void newVar) {
+    qualifications = newVar;
+  }
+
+  /**
+   * Get the value of qualifications
+   * @return the value of qualifications
+   */
+  private void getQualifications () {
+    return qualifications;
+  }
+
+  /**
+   * Set the value of login_details
+   * @param newVar the new value of login_details
+   */
+  private void setLogin_details (void newVar) {
+    login_details = newVar;
+  }
+
+  /**
+   * Get the value of login_details
+   * @return the value of login_details
+   */
+  private void getLogin_details () {
+    return login_details;
+  }
+
+  /**
+   * Set the value of fees
+   * @param newVar the new value of fees
+   */
+  private void setFees (void newVar) {
+    fees = newVar;
+  }
+
+  /**
+   * Get the value of fees
+   * @return the value of fees
+   */
+  private void getFees () {
+    return fees;
+  }
+
+  /**
+   * Set the value of exam_dates
+   * @param newVar the new value of exam_dates
+   */
+  private void setExam_dates (void newVar) {
+    exam_dates = newVar;
+  }
+
+  /**
+   * Get the value of exam_dates
+   * @return the value of exam_dates
+   */
+  private void getExam_dates () {
+    return exam_dates;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verification()
+  {
+  }
+
+
+  /**
+   */
+  public void processing()
+  {
+  }
+
+
+  /**
+   */
+  public void registration()
+  {
+  }
+
+
+}
---
+++ student.java
@@ -0,0 +1,130 @@
+
+
+/**
+ * Class student
+ */
+public class student {
+
+  //
+  // Fields
+  //
+
+  private void Name;
+  private void dob;
+  private void phone_no;
+  private void email_id;
+  private void subject;
+  
+  //
+  // Constructors
+  //
+  public student () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Name
+   * @param newVar the new value of Name
+   */
+  private void setName (void newVar) {
+    Name = newVar;
+  }
+
+  /**
+   * Get the value of Name
+   * @return the value of Name
+   */
+  private void getName () {
+    return Name;
+  }
+
+  /**
+   * Set the value of dob
+   * @param newVar the new value of dob
+   */
+  private void setDob (void newVar) {
+    dob = newVar;
+  }
+
+  /**
+   * Get the value of dob
+   * @return the value of dob
+   */
+  private void getDob () {
+    return dob;
+  }
+
+  /**
+   * Set the value of phone_no
+   * @param newVar the new value of phone_no
+   */
+  private void setPhone_no (void newVar) {
+    phone_no = newVar;
+  }
+
+  /**
+   * Get the value of phone_no
+   * @return the value of phone_no
+   */
+  private void getPhone_no () {
+    return phone_no;
+  }
+
+  /**
+   * Set the value of email_id
+   * @param newVar the new value of email_id
+   */
+  private void setEmail_id (void newVar) {
+    email_id = newVar;
+  }
+
+  /**
+   * Get the value of email_id
+   * @return the value of email_id
+   */
+  private void getEmail_id () {
+    return email_id;
+  }
+
+  /**
+   * Set the value of subject
+   * @param newVar the new value of subject
+   */
+  private void setSubject (void newVar) {
+    subject = newVar;
+  }
+
+  /**
+   * Get the value of subject
+   * @return the value of subject
+   */
+  private void getSubject () {
+    return subject;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void form_fillling()
+  {
+  }
+
+
+  /**
+   */
+  public void registration()
+  {
+  }
+
+
+}
