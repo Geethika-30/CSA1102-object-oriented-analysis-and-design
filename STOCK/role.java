e09f71751d429f53d60bef87069ad36aac1f67c0
Geethika-30 <112071177+Geethika-30@users.noreply.github.com>
2023-08-21 14:00:21 +0530

    Add files via upload

---
+++ STOCK/role.java
@@ -0,0 +1,103 @@
+
+
+/**
+ * Class role_
+ */
+public class role_ {
+
+  //
+  // Fields
+  //
+
+  private void _id;
+  private void title;
+  private void _description;
+  
+  //
+  // Constructors
+  //
+  public role_ () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of _id
+   * @param newVar the new value of _id
+   */
+  private void set_id (void newVar) {
+    _id = newVar;
+  }
+
+  /**
+   * Get the value of _id
+   * @return the value of _id
+   */
+  private void get_id () {
+    return _id;
+  }
+
+  /**
+   * Set the value of title
+   * @param newVar the new value of title
+   */
+  private void setTitle (void newVar) {
+    title = newVar;
+  }
+
+  /**
+   * Get the value of title
+   * @return the value of title
+   */
+  private void getTitle () {
+    return title;
+  }
+
+  /**
+   * Set the value of _description
+   * @param newVar the new value of _description
+   */
+  private void set_description (void newVar) {
+    _description = newVar;
+  }
+
+  /**
+   * Get the value of _description
+   * @return the value of _description
+   */
+  private void get_description () {
+    return _description;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void addrole()
+  {
+  }
+
+
+  /**
+   */
+  public void editrole()
+  {
+  }
+
+
+  /**
+   */
+  public void deleterole()
+  {
+  }
+
+
+}
---
+++ STOCK/user.java
@@ -0,0 +1,103 @@
+
+
+/**
+ * Class user_
+ */
+public class user_ {
+
+  //
+  // Fields
+  //
+
+  private void Id;
+  private void _name;
+  private void ROLE;
+  
+  //
+  // Constructors
+  //
+  public user_ () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Id
+   * @param newVar the new value of Id
+   */
+  private void setId (void newVar) {
+    Id = newVar;
+  }
+
+  /**
+   * Get the value of Id
+   * @return the value of Id
+   */
+  private void getId () {
+    return Id;
+  }
+
+  /**
+   * Set the value of _name
+   * @param newVar the new value of _name
+   */
+  private void set_name (void newVar) {
+    _name = newVar;
+  }
+
+  /**
+   * Get the value of _name
+   * @return the value of _name
+   */
+  private void get_name () {
+    return _name;
+  }
+
+  /**
+   * Set the value of ROLE
+   * @param newVar the new value of ROLE
+   */
+  private void setROLE (void newVar) {
+    ROLE = newVar;
+  }
+
+  /**
+   * Get the value of ROLE
+   * @return the value of ROLE
+   */
+  private void getROLE () {
+    return ROLE;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void adduser()
+  {
+  }
+
+
+  /**
+   */
+  public void edit_user()
+  {
+  }
+
+
+  /**
+   */
+  public void deleteuser()
+  {
+  }
+
+
+}
