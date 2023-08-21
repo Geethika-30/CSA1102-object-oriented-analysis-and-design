e7231c7bed233f9c2923e47229a10316ac68869a
Geethika-30 <112071177+Geethika-30@users.noreply.github.com>
2023-08-21 13:12:52 +0530

    Add files via upload

---
+++ conference management system/ORGANIZERS.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class ORGANIZERS
+ */
+public class ORGANIZERS {
+
+  //
+  // Fields
+  //
+
+  private void Payment;
+  private void feedback;
+  
+  //
+  // Constructors
+  //
+  public ORGANIZERS () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Payment
+   * @param newVar the new value of Payment
+   */
+  private void setPayment (void newVar) {
+    Payment = newVar;
+  }
+
+  /**
+   * Get the value of Payment
+   * @return the value of Payment
+   */
+  private void getPayment () {
+    return Payment;
+  }
+
+  /**
+   * Set the value of feedback
+   * @param newVar the new value of feedback
+   */
+  private void setFeedback (void newVar) {
+    feedback = newVar;
+  }
+
+  /**
+   * Get the value of feedback
+   * @return the value of feedback
+   */
+  private void getFeedback () {
+    return feedback;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void handlesPayment()
+  {
+  }
+
+
+  /**
+   */
+  public void givesFeedback()
+  {
+  }
+
+
+}
